fe900717ed09405af2499083e480b67d0381bb2f
Ragahvendra Achar <raghavendra.achar@lilly.com>
2026-04-20 16:40:02 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const basePath = repo ? `/${repo}` : '';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
